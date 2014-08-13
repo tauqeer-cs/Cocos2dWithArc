bb91f97927cedac29328e83d10baa67204c0907b
Tauqeer Ahmed <tauqeer99@gmail.com>
2014-08-13 11:22:10 +0500

    Files
    files

---
+++ Tensed/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Tensed
+//
+//  Created by Tauqeer Ahmed on 8/13/14.
+//  Copyright Merrycode 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+        return retVal;
+    }
+}
---
+++ cocos2d-libraryTests/cocos2d_libraryTests.m
@@ -0,0 +1,34 @@
+//
+//  cocos2d_libraryTests.m
+//  cocos2d-libraryTests
+//
+//  Created by Tauqeer Ahmed on 8/13/14.
+//  Copyright (c) 2014 Merrycode. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface cocos2d_libraryTests : XCTestCase
+
+@end
+
+@implementation cocos2d_libraryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
